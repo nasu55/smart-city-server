82cb4b980ca98efe2c880fd718d6995fb3d3de84
nasu55 <timannan93@gmail.com>
2024-12-06 19:45:25 +0530

    changes saved

--- src/controllers/dashboard/Categorycontroller.js
+++ src/controllers/dashboard/Categorycontroller.js
@@ -1,3 +1,23 @@
+export const createData = async(req, res) => {
+try{
+    const { category_id, username, password, email_id } = req.body;
+    await StaffModel.create({
+        category_id: category_id,
+        username: username,
+        password: password,
+        email_id: email_id,
+    });
+    return res.status(200).json({
+        success: true,
+        message: 'Created Successfull!',
+    });
+} catch (error){
+    return res.status(500).json({
+        success: false,
+        message: error.message,
+    });
+}
+}
 export const updateData = async (req, res) => {
     try{
         const productId = req.params.id;
@@ -29,6 +49,31 @@ export const deleteData = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: 'Server error'
+        });
+    }
+};
+export const viewData = async (req, res) => {
+    try{
+        const productId = req.params.id;
+        const product = await TestModel.findById(productId);
+        return res.status(200).json({
+            success: true,
+            message: 'Fetched',
+            data: { product: product },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: 'Server error',
+        });
+    }
+};
+export const getAllData = async (req, res) => {
+    try{
+        const products = awaits TestModel.find();
+        return res.status(200).json({
+            success: false,
+            message: 'Server error',
         });
     }
 };
