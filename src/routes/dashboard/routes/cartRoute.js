be5d3a5180bdb251601b46888f0d391ebfab264a
nasu55 <timannan93@gmail.com>
2025-01-31 10:47:12 +0530

    edd

--- src/routes/dashboard/routes/cartRoute.js
+++ src/routes/dashboard/routes/cartRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createCart } from "../../../controllers/dashboard/CartController.js";
+import { createCart } from "../../../controllers/frontend/cartController.js";
 
 
 export const cartRoute = express.Router();
