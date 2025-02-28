19cc2fd9296774773ff466fa0024abf886a5d688
Muhd-Asif <asifmuhammed005@gmail.com>
2025-02-28 10:16:39 +0530

    push

--- src/routes/frontend/routes/homeRoute.js
+++ src/routes/frontend/routes/homeRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getHomePage, getLocalities } from "../../../controllers/frontend/homeController.js";
+import { authMiddleware } from "../../../middleware/UserMiddleware.js";
 
 
 export const homeRoute = express.Router();
-homeRoute.get('/',getHomePage); 
+homeRoute.get('/',authMiddleware, getHomePage); 
 homeRoute.get('/localities',getLocalities); 
 
